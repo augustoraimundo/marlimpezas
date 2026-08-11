19b51be044d27384498ede96249d4ce59c46d02c
Augusto Baptista <augustoraimundobaptistagugas@gmail.com>
2026-08-11 12:10:04 +0100

    Update index.js

--- marlimpezaSite/scripts/index.js
+++ marlimpezaSite/scripts/index.js
@@ -2,7 +2,9 @@ import animaScroll from "./modules/animaScroll.js";
 import list from "./modules/listView.js";
 import numbers from "./modules/animaNumero.js";
 import contactForm from "./modules/contactForm.js";
+import navToggle from "./modules/navToggle.js";
 animaScroll()
 list()
 numbers()
 contactForm()
+navToggle()
